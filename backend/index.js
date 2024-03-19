1fa53f7b2434298eded84bc97c549db7b97eba06
Nomad-Rob <rfarley89@gmail.com>
2024-03-19 11:24:36 -0500

    Quck add

--- backend/index.js
+++ backend/index.js
@@ -25,4 +25,27 @@ app.post("/authenticate", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // console.log("Fetch user from DB.");
+  // return res.json({ user: {} });
+
+  // Fetch this user from Chat Engine in this project!
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": process.env.CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// Docs at rest.chatengine.io
+// vvv On port 3001!
 app.listen(3001);
